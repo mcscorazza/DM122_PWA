06e325f27bdfbd718ddf498aaf3f270e3981f58c
Marcos <mcs.corazza@gmail.com>
2025-11-06 20:27:44 -0300

    feat: created styles.css

--- src/modules/app.js
+++ src/modules/app.js
@@ -6,6 +6,7 @@ class App {
     this.#registerServiceWorker();
     const gymLogService = new GymLogService();
     window.HTMLService = new HTMLService(gymLogService);
+    this.navigate('screen-1', 'Minhas Rotinas');
   }
   #registerServiceWorker() {
     navigator.serviceWorker
@@ -13,6 +14,24 @@ class App {
       .then(console.log(`🚩 [app.js] SW registered`))
       .catch(console.log(`🚩 [app.js] SW failed to register`));
   }
+  navigate(pageId, title, routineId = null) {
+  pages.forEach(page => page.classList.remove('active'));
+
+  const targetPage = document.getElementById(pageId);
+  if (targetPage) {
+    targetPage.classList.add('active');
+  }
+
+  headerTitle.textContent = title;
+
+  if (pageId === 'screen-1') {
+    btnHeaderBack.classList.add('hidden');
+    btnHeaderEdit.classList.remove('hidden');
+  } else {
+    btnHeaderBack.classList.remove('hidden');
+    btnHeaderEdit.classList.add('hidden');
+  }
+}
 }
 
 new App();
